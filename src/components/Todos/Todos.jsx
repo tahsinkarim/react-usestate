408de09bec1f2c2c48ff82395c596ff9afee8dc2
tahsinkarim <tahsinnkarim@gmail.com>
2022-09-23 16:48:30 +0600

    Added Todos api

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Todos.css";
 
 const Todos = () => {
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h1>Todos</h1>
+      {users.map((user) => (
+        <div className={user.completed ? "users" : "users completed"}>
+          <p>
+            {user.title} : {user.completed ? "Completed" : "Not Completed"}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Todos;
